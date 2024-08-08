51eedc8e6e4fd81512e365d57dbfe6b271fa8654
adarshjha7050 <jhaa63653@gmail.com>
2024-08-08 15:05:18 +0530

    first commit

---
+++ adarsh2.js
@@ -0,0 +1,31 @@
+//Find length of array
+let rollNumber =[100,101,102,103,104,105];
+console.log("Length of Array :-> ",rollNumber.length);
+   
+//Converts into string 
+
+rollNumber.toString();
+console.log("Converts into string :-> ",rollNumber);
+
+
+//Add element into into array using push method
+rollNumber.push(5,10);     
+console.log( "Add element using push method :-> ",rollNumber);
+
+//Pop remove single element from array 
+rollNumber.pop();
+console.log( "Remove element using pop :-> ",rollNumber);
+
+//Concat
+let join_Arr = rollNumber.concat("Adarsh" , "Pramod");
+console.log("Add two Array using concat method :-> " ,join_Arr);
+
+
+
+
+
+
+
+
+
+
